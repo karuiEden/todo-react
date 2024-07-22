968be502f59dce4600f40c489f04a82d780e214b
starikov.ma <starikov.ma@dozen.mephi.ru>
2024-07-22 15:04:36 +0300

    Initial commit

---
+++ src/head.tsx
@@ -0,0 +1,10 @@
+export default function laHeader(){
+    return(
+        <header className="App-header">
+            <h3>LA TODO</h3>
+            <nav className="App-nav">
+                <a href=""
+            </nav>
+        </header>
+    )
+}
